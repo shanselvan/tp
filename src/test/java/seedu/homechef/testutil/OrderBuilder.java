be2b391a692c3ee7196efb6f8eb4b397717ec61a
Joshua  Teo <Joshuakxteo@gmail.com>
2026-03-13 16:41:42 +0800

    feat: add optional paymentInfo field to Order

--- src/test/java/seedu/homechef/testutil/OrderBuilder.java
+++ src/test/java/seedu/homechef/testutil/OrderBuilder.java
@@ -1,6 +1,7 @@
 package seedu.homechef.testutil;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import seedu.homechef.model.order.Address;
@@ -9,6 +10,7 @@ import seedu.homechef.model.order.Email;
 import seedu.homechef.model.order.Food;
 import seedu.homechef.model.order.Name;
 import seedu.homechef.model.order.Order;
+import seedu.homechef.model.order.PaymentInfo;
 import seedu.homechef.model.order.Phone;
 import seedu.homechef.model.tag.DietTag;
 import seedu.homechef.model.util.SampleDataUtil;
@@ -32,6 +34,7 @@ public class OrderBuilder {
     private Address address;
     private Date date;
     private Set<DietTag> dietTags;
+    private Optional<PaymentInfo> paymentInfo = Optional.empty();
 
     /**
      * Creates a {@code OrderBuilder} with the default details.
@@ -57,6 +60,7 @@ public class OrderBuilder {
         address = orderToCopy.getAddress();
         date = orderToCopy.getDate();
         dietTags = new HashSet<>(orderToCopy.getTags());
+        paymentInfo = orderToCopy.getPaymentInfo();
     }
 
     /**
@@ -115,8 +119,19 @@ public class OrderBuilder {
         return this;
     }
 
+    /**
+     * Sets the {@code PaymentInfo} of the {@code Order} that we are building.
+     */
+    public OrderBuilder withPaymentInfo(PaymentInfo paymentInfo) {
+        this.paymentInfo = Optional.of(paymentInfo);
+        return this;
+    }
+
+    /**
+     * Builds and returns the {@code Order}.
+     */
     public Order build() {
-        return new Order(food, name, phone, email, address, date, dietTags);
+        return new Order(food, name, phone, email, address, date, dietTags, paymentInfo);
     }
 
 }
