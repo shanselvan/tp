21c49960da323c9b9d1aeb75b1c1fc756be4a335
Shan Selvan <skillfulshan@gmail.com>
2026-04-13 23:09:37 +0800

    Modify parse method for receipt command

--- src/main/java/seedu/homechef/logic/parser/ReceiptCommandParser.java
+++ src/main/java/seedu/homechef/logic/parser/ReceiptCommandParser.java
@@ -1,7 +1,5 @@
 package seedu.homechef.logic.parser;
 
-import static seedu.homechef.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-
 import seedu.homechef.commons.core.index.Index;
 import seedu.homechef.logic.commands.ReceiptCommand;
 import seedu.homechef.logic.parser.exceptions.ParseException;
@@ -17,7 +15,7 @@ public class ReceiptCommandParser implements Parser<ReceiptCommand> {
             Index index = ParserUtil.parseIndex(args);
             return new ReceiptCommand(index);
         } catch (ParseException pe) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ReceiptCommand.MESSAGE_USAGE), pe);
+            throw ParserUtil.handleIndexParseException(pe, ReceiptCommand.MESSAGE_USAGE);
         }
     }
 }
